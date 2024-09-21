88bd1aa2ba9b1010adb6d0409d6f5499ea70c203
JaseelaAlimahamed <jessibrototype@gmail,com>
2024-09-21 19:22:38 +0530

    bug: cleared

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
-import UserRoutes from './routes/UserRoutes';
+import ProviderRoutes from './routes/ProviderRoutes';
 // import'./App.css';
+
 function App() {
 
 
   return (
     <Routes>
-      <Route path="/*" element={<ProviderRoutes/>} />
+      <Route path="/*" element={<ProviderRoutes />} />
     </Routes>
   );
 }
