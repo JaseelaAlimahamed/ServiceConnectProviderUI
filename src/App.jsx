4ad09d72eec5e744b6e245ea217b12e58824ef77
Hariasuresh <Hariasuresh1@gmail.com>
2024-09-25 17:02:43 +0530

    added decline page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 
+import DeclinePage from './pages/Services/DeclinePage';
 
-import ProviderRoutes from './routes/ProviderRoutes';
+// import ProviderRoutes from './routes/ProviderRoutes';
 // import'./App.css';
 function App() {
 
   return (
    <>
 
-    <Routes>
-      <Route path='/*' element={<ProviderRoutes/>}/>
-    </Routes>
+  <DeclinePage/>
 
-
- 
+ </>
   );
 }
 
 export default App;
+
+{/* <Routes>
+<Route path='/*' element={<DeclinePage/>}/>
+</Routes> */}
---
+++ src/pages/Services/DeclinePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DeclineForm from '../../components/serviceProvider/complaintDecline/ComplaintForm'
+
+const DeclinePage = () => {
+  return (
+    <div>
+        <DeclineForm/>
+    </div>
+  )
+}
+
+export default DeclinePage;
