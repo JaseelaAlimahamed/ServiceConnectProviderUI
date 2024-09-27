574644adcc16b5bb887909bc0949cb0253bd2ce2
rollinggir <joannetsengannu@gmail.com>
2024-09-27 20:23:55 +0530

    logic changed as described for modal

--- src/pages/ServiceProviderPages/LeadDetails.jsx
+++ src/pages/ServiceProviderPages/LeadDetails.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
-import Navbar from '../../components/reUsableComponents/ServiceRequestList/Navbar'
 import eclipse from '../../assets/images/Ellipse309.png'
 import { FaLocationDot } from "react-icons/fa6";
 import Button from '../../components/reUsableComponents/ServiceRequestList/Button';
 import shape from '../../assets/images/shape.png'
 import { useNavigate } from 'react-router-dom';
-import CallModal from '../../components/reUsableComponents/CallModal';
+import ReactModal from 'react-modal';
 const LeadDetails = () => {
     const [tab,setTab]=useState(false)
     const [open,setOpen]=useState(false)
+    ReactModal.setAppElement('#root');
+
     const booking={
         bookingId:'12aa21w',
         title:'Bulb Change',
@@ -34,9 +35,21 @@ const handleDecline=()=>{
     navigate('/')
 
 }
-const handleCall=()=>{
+const handleAddress=()=>{
     setOpen(true)
+    
 
+}
+const handleCall=()=>{
+
+}
+const handleCancel=()=>{
+    setTab(true)
+    setOpen(false)
+}
+const handleOk=()=>{
+    setTab(false)
+    setOpen(false)
 }
   return (
     <div className='bg-light-gray font-daytime h-screen py-10'>
@@ -54,12 +67,12 @@ const handleCall=()=>{
         </div>
         <p className='text-daytime text-sm mb-10 pl-4 pt-4'>Booking id: {booking.bookingId}</p>
         
-            <div className="container mx-auto flex flex-col justify-center items-center w-full shadow-2xl  bg-primary rounded-2xl ">
+            <div className="container mx-auto flex flex-col justify-center items-center  shadow-2xl w-8/12 max-lg:w-full  bg-primary rounded-2xl ">
                 <div className="flex justify-between w-full border-b-2 px-3  border-light-gray font-semibold">
                     <div className={`flex justify-center items-center w-full cursor-pointer ${tab?'border-b-2 ':''} py-2 border-cardfont`}  onClick={()=>setTab(true)}>
                         <h3>Request</h3>
                     </div>
-                    <div className={`flex justify-center items-center w-full cursor-pointer ${!tab?'border-b-2 ':''} py-2 border-cardfont`} onClick={()=>setTab(false)}>
+                    <div className={`flex justify-center items-center w-full cursor-pointer ${!tab?'border-b-2 ':''} py-2 border-cardfont`} onClick={handleAddress}>
                         <h3>Address</h3>
                     </div>
                 </div>
@@ -79,8 +92,8 @@ const handleCall=()=>{
                         <div className="py-2">
                             <h3 className='font-medium'>Images & Video</h3>
                             {
-                                booking.images.map((image)=>
-                                <img src={image} alt="" className='w-20 h-20 rounded-full aspect-square object-cover'/>
+                                booking.images.map((image,index)=>
+                                <img key={index} src={image} alt="" className='w-20 h-20 rounded-full aspect-square object-cover'/>
                                 )
                             }
                         </div>
@@ -130,7 +143,45 @@ const handleCall=()=>{
             </div>
             </div>
             <Button label={tab?'Decline':'Call'} color={tab?'declainbtn':'statusopened'} width='300px' onClick={tab?handleDecline:handleCall} />
-            <CallModal isOpen={open} onClose={()=>setOpen(false)} number={user.phone}/>
+            <ReactModal
+      isOpen={open}
+      onRequestClose={()=>setOpen(false)}
+      
+      style={{
+        overlay: {
+         margin:'',
+         backgroundColor:'transparent',
+          backdropFilter: 'blur(5px)', 
+        },
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          transform: "translate(-50%, -50%)",
+          width: "90%", 
+          maxWidth: "500px", 
+          padding: "20px", 
+          borderRadius: "10px",
+          textAlign: "center",
+          backgroundColor: "#D9D9DB",
+          
+        },
+      }}
+    >
+     
+      <h2 className='font-heading font-medium tracking-wide text-lg mb-4' style={{ wordWrap: 'break-word', lineHeight: '1.5em' }}>
+        By attending your lead, your balance will be reduced by one.
+      </h2>
+      
+     
+      <div className="flex gap-2 justify-center">
+
+      <Button label={'Okay'} color={'dark-gray'} onClick={handleOk} width={'126px'} />
+      <Button label={'Cancel'} color={'dark-gray'} onClick={handleCancel} width={'126px'}/>
+      </div>
+
+    </ReactModal>
         </div>
 
     
