3d1638287baac198fe416d5acc7c5cf275e35e11
bijo <bijoksanoai@gmail.com>
2024-10-30 12:31:56 +0530

    bookings details View Bijoy

--- src/baseUrl/axiosInstance.js
+++ src/baseUrl/axiosInstance.js
@@ -1,7 +1,26 @@
 import axios from "axios";
 
+// Assuming the token is stored in local storage after user login
+const token = localStorage.getItem("authToken");
+
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8000/",
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMwMjcyMTExLCJpYXQiOjE3MzAyNzAzMTEsImp0aSI6IjU2NmY2MGZlODExZDRkY2ZiZWZjNWY1MjMyNGI2NDZlIiwidXNlcl9pZCI6NH0.ePBMEV5HAPhGE30vqt40URLJ0Nm0C2uIXoExGb9ebd4`,
+
+  },
+});
+
+// Add the token dynamically for each request if it exists
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken"); // Fetch the token from local storage
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default axiosInstance;
--- src/service_providers_apis/api.js
+++ src/service_providers_apis/api.js
@@ -1,19 +1,2 @@
-// import axiosInstance from "../../baseUrl/axiosInstance";
 
-//  Fetch Subcategories details
-
-// export const fetchSuggestions = async (category_id) => {
-//   try {
-//     const response = await axiosInstance.get(
-//       `categories/${category_id}/subcategories/`,
-//       {
-//         headers: { "Content-Type": "application/json" },
-//       }
-//     );
-//     console.log(response);
-//     return Array.isArray(response.data) ? response.data : [];
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     return [];
-//   }
-// };
+  
